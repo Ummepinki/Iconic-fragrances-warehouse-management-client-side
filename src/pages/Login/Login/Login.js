c3caa2b3e837f02dc32068812bc65c4c2f97beed
Ummepinki <salmaumme888@gmail.com>
2022-05-09 22:26:32 +0600

    login form and register added

--- src/pages/Login/Login/Login.js
+++ src/pages/Login/Login/Login.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const navigate = useNavigate();
+    const handleSubmit = event => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        console.log(email, password);
+
+    }
+
+    const navigateRegister = event => {
+        navigate('/register');
+    }
     return (
-        <div>
-            <h2>Please login</h2>
+        <div className='container w-50 mx-auto'>
+            <h2 className='text-dark text-center mt-2'>Please login</h2>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
+                </Form.Group>
+
+                <Button variant="dark" type="submit">
+                    Submit
+                </Button>
+            </Form>
+            <p>Are you new? <span className='text-primary' onClick={navigateRegister}> please register</span></p>
         </div>
     );
 };
